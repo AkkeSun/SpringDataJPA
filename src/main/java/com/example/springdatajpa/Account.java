77e778bdf10b3f081d3d6b1746dc9a63be1b61fa
AkkeSun <akkessun@gmail.com>
2021-09-30 17:56:47 +0900

    0930(2)

--- src/main/java/com/example/springdatajpa/Account.java
+++ src/main/java/com/example/springdatajpa/Account.java
@@ -3,17 +3,49 @@ package com.example.springdatajpa;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Getter @Setter
-@Entity // 해당 클래스와 동일한 테이블과 매핑하겠다
+@Entity(name = "Account") // Java에서 객채 명 (default = 클래스명)
+@Table(name = "Account")  // DB 테이블명. 생략가능 (default = Entity명)
 public class Account {
 
-    @Id @GeneratedValue
+    /*
+    @Id
+        Primary Key 필드 지정
+
+    @GeneratedValue
+        Primary Key의 기본값을 자동생성할 떄 사용
+        GenerationType.AUTO : DB 방언에 따라 자동지정 (default)
+        GenerationType.SEQUENCE : DB 시퀀스를 이용해서 키 값 생성
+        GenerationType.IDENTITY : DB에 키 생성방법을 위임
+
+     @Column
+        생략가능. 컬럼의 추가 제약을 줄 때 지정
+
+     @Temporal
+        날짜와 시간 타입을 매핑할 때 사용
+        현재 JPA2.1까지는 Date와 Calendar만 사용 가능
+
+    @Transient
+        컬럼으로 맵핑하고 싶지않은 변수 선언
+     */
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    @Column
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column
     private String password;
 
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @Transient
+    private String secret;
 }
---
+++ src/main/java/com/example/springdatajpa/JpaRunner.java
@@ -0,0 +1,37 @@
+package com.example.springdatajpa;
+
+import org.hibernate.Session;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.Date;
+
+@Component
+@Transactional // 한 트랜잭션 내에서 쿼리 작업이 이루어지게 설정 (method에 붙여도 됨)
+public class JpaRunner implements ApplicationRunner {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Account account = new Account();
+        account.setUsername("exg");
+        account.setPassword("213213123");
+        account.setDate(new Date());
+        account.setSecret("data");
+
+        // 영속화
+        entityManager.persist(account);
+
+        // 영속화 후 바로 세션 저장 (org.hibernate.Session)
+        /*
+        Session session = entityManager.unwrap(Session.class);
+        session.save(account);
+        */
+    }
+}
